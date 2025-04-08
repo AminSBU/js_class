ce9cac7dd7d10a4f70a3548b38d7519ccff67ea0
LAPTOP-8L1MTC96\Afra <omid.elc88@gmail.com>
2025-04-08 22:31:08 +0330

    add console.log for debug

--- 10_todo_project/todo.js
+++ 10_todo_project/todo.js
@@ -10,3 +10,15 @@ function addtodoNotReal()
 
     input.value = '';
 }
+
+const task = ['make breakfast', 'eat lunch', 'drink dinner'];
+console.log(task);
+const taskDiv = document.querySelector('.showTask');
+console.log(taskDiv);
+
+for(let index = 0; index < 3; index++){
+    const taskk = task[index];
+    console.log(taskk);
+    const html = `<p>${taskk}</p>`;
+    console.log(html);
+}
