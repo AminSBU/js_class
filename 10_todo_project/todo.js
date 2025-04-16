39359406adaefe61f64f93b8c274b42052742d48
LAPTOP-8L1MTC96\Afra <omid.elc88@gmail.com>
2025-04-17 00:29:28 +0330

    consider delete function to remove task from list

--- 10_todo_project/todo.js
+++ 10_todo_project/todo.js
@@ -22,17 +22,39 @@ function addTasks(){
 }
 
 
-function todoTasks(){
-    const inputTask = document.querySelector('.todoInput');
-    const inputDate = document.querySelector('.todoDate');
-    const showTask  = document.querySelector('.todoTasks');
-    
-    const listTask  = inputTask.value;
-    const dateTask  = inputDate.value;
+function todoTasks() {  
+    const inputTask = document.querySelector('.todoInput');  
+    const inputDate = document.querySelector('.todoDate');  
+    const showTask  = document.querySelector('.todoTasks');  
 
-    const htmlCode = `<p>${listTask}${dateTask ? `, ${dateTask}` : ''}</p><button class="deleteTaskButton">Delete</button>`;
-    showTask.innerHTML += htmlCode;
-}
+    const listTask  = inputTask.value;  
+    const dateTask  = inputDate.value;  
+
+    // Create a new task item  
+    const taskItem = document.createElement('div');  
+    taskItem.classList.add('taskItem');  
+
+    // Create the HTML structure for the task  
+    taskItem.innerHTML = `  
+        <p>${listTask}${dateTask ? `, ${dateTask}` : ''}</p>  
+        <button class="deleteTaskButton" onclick="deleteTaskFunction(this)">Delete</button>  
+    `;  
+
+    // Append the task item to the showTask container  
+    showTask.appendChild(taskItem);  
+
+    // Clear inputs after adding a task  
+    inputTask.value = '';  
+    inputDate.value = '';  
+}  
+
+function deleteTaskFunction(button) {  
+    // Get the parent element of the button, which is the task item  
+    const taskItem = button.parentElement;  
+    
+    // Remove the task item from the DOM  
+    taskItem.remove();  
+}  
 
 const task = ['make breakfast', 'eat lunch', 'drink dinner'];
 const taskDiv = document.querySelector('.showTask');
